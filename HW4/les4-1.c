74e58d8c8b7d462866d291ecdd0c802fdf8c0e65
Ilfat Rifovich <gizatullin_ilfat@inbox.ru>
2024-04-20 12:28:11 +0700

    first commit

---
+++ HW4/les4-1.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    int a,b;
+    scanf("%u%u", &a,&b);
+    if(a>b)
+        printf("%u%u", a,b);
+    else
+        printf("%u%u", b,a);
+}
+
---
+++ HW4/les4-3.c
@@ -0,0 +1,22 @@
+
+
+
+#include <inttypes.h>
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    unsigned int x;
+    unsigned int a,b,c;
+    scanf("%u", &x);
+    a=x&0xF;
+    b=(x>>4)&0xF;
+    c=(x>>8);
+    a=a>b?a:b;
+    a=a>c?a:c;
+    printf("%u", a);
+    return 0;
+
+}
+
